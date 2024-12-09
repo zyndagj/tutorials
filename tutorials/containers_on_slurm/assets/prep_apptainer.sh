01316d1f2203bd73d6f83204dbeb547fb1d91635
gzynda-nvidia <gzynda@roundbook.local>
2024-12-09 10:40:41 -0800

    Updated queue

--- tutorials/containers_on_slurm/assets/prep_apptainer.sh
+++ tutorials/containers_on_slurm/assets/prep_apptainer.sh
@@ -1,5 +1,5 @@
 # Start a 3 hour interactive job with 1 GPU
-srun -p gpu -n 1 -G 1 --cpus-per-task 16 -t 03:00:00 --pty bash -l
+srun -p interactive -n 1 -G 1 --cpus-per-task 16 -t 03:00:00 --pty bash -l
 
 # Change cachedir to /tmp
 export APPTAINER_CACHEDIR=/tmp/${USER}_apptainer_cache
@@ -8,6 +8,8 @@ export APPTAINER_CACHEDIR=/tmp/${USER}_apptainer_cache
 mkdir -p ${MYDATA}/containers
 
 # Change to your workspace
+#  - This is a good place to store containers
+#  - Keep definition files on $HOME
 cd ${MYDATA}/containers
 
 # Pull the container
